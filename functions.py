0fa4d47078d1811ee497531dfe7f882ddbe04cc5
Mikhail Kovalyshev <kovalyshev.mikhail@gmail.com>
2021-10-28 22:49:04 +0300

    Added get_routes

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import requests
 import datetime as dt
+from bs4 import BeautifulSoup
+import json
+import os
 
 TODAY = dt.datetime.today().strftime('%Y-%m-%d')
 HOST = 'https://www.bustime.ru'
@@ -9,3 +12,38 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/87.0.4280.141 Safari/537.36'
 }
+
+
+def get_cities(url: str = 'https://www.bustime.ru') -> None:
+    """
+    gets list of cities' hrefs from bustime.ru
+    writes to resources/cities.txt
+    returns None
+    """
+
+    soup = BeautifulSoup(requests.get(url).text)
+
+    cities = [x.get('href') for x in soup.find("div", {"aria-label": " Список городов "}). \
+        find_all("a", {"class": 'item'})]
+
+    with open("resources/cities.txt", "w") as file:
+        file.write(';'.join(cities))
+
+
+def get_routes(city: str) -> None:
+    """
+    gets dict of route ids matched with route names
+    writes to resources/*city*/routes.json
+    return None
+    """
+
+    soup = BeautifulSoup(requests.get(HOST + city + 'transport/' + TODAY).text)
+
+    routes = {int(x.get('value')): x.text for x in soup.find('select', {'name': 'bus_id'}). \
+        find_all('option') if x.get('value') != '0'}
+
+    if not os.path.exists('resources/' + city.strip('/')):
+        os.mkdir('resources/' + city.strip('/'))
+
+    with open('resources/' + city.strip('/') + '/routes.json', 'w', encoding='utf-8') as file:
+        json.dump(routes, file, ensure_ascii=False)
