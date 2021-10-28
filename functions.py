ee540ea9e4e01f5553b67a5e3623078408afa615
Mikhail Kovalyshev <kovalyshev.mikhail@gmail.com>
2021-10-28 22:09:02 +0300

    Added requirements.txt, added constants

---
+++ functions.py
@@ -0,0 +1,11 @@
+import requests
+import datetime as dt
+
+TODAY = dt.datetime.today().strftime('%Y-%m-%d')
+HOST = 'https://www.bustime.ru'
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+              'application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/87.0.4280.141 Safari/537.36'
+}
